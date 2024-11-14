2d17be2e269c6f750567c46e79bc51c199817093
Pauline122112 <pauline.design007@gmail.com>
2024-11-13 17:53:27 -0700

    added index.html and main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/App.css';  // Ensure you are importing your global styles
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
