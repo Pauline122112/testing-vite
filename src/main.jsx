f16fa925a8b0045dd6ed3805718cd5d79349c295
Pauline122112 <pauline.design007@gmail.com>
2024-11-13 17:57:55 -0700

    made changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/App.css';  // Ensure you are importing your global styles
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/App.css'  // Ensure you are importing your global styles
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(<App />)
